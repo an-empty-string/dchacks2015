d91ade65e5512cbd11baff8708e79e3b2fdddacb
Fox Wilson <foxwilson123@gmail.com>
2015-08-01 16:17:45 -0400

    fix config

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ with open("config.json") as f:
     config = json.load(f)
 
 redis_info = {"host": config["REDIS_HOST"], "password": config["REDIS_PASSWORD"]}
-wmata = utils.metro.MetroApi(config["API_KEY"], **redis_info)
+wmata = utils.metro.MetroApi(config["API_KEY"], redis_info)

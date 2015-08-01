3ada12cf7812db525b86a111b25b53669e3cc6fc
laudiacay <laudiacay@gmail.com>
2015-08-01 17:32:14 -0400

    calculating the entire map of points with a dict :D

--- scripts/algo.py
+++ scripts/algo.py
@@ -11,14 +11,25 @@ def find_dist(stat, lata, longa):
 
 def find_add(stat, lata, longa):
     wa = find_dist(stat, lata, longa)
-    const = 1
+    const = 1  # fix this later- this is what you need to solve!!!
     return const/(wa**2)
 
 
-def point_calc(lata,longa):
+def point_calc(lata, longa):
     i = 0.0
     for s in wmata.lines:
         for staa in s:
             i += find_add(staa, lata, longa)
     return i
 
+
+def point_map(start_lat, stop_lat, start_lon, stop_lon, resolution):
+    latlist = list(f_range(start_lat, stop_lat, resolution))
+    lonlist = list(f_range(start_lon, stop_lon, resolution))
+    return {lat: {lon: point_calc(lat, lon) for lon in lonlist} for lat in latlist}
+
+
+def f_range(mini, maxi, step):
+    while mini < maxi:
+        yield mini
+        mini += step

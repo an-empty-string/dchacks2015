8fc2bd0db61bfcb01be992eb592596107d75a4c1
laudiacay <laudiacay@gmail.com>
2015-08-01 17:07:52 -0400

    changed variable name

--- scripts/algo.py
+++ scripts/algo.py
@@ -2,18 +2,23 @@ __author__ = 'claudia'
 from config import wmata
 from math import hypot
 
-def finddist(stat, lata, longa):
+
+def find_dist(stat, lata, longa):
     dx = stat.location.lon - longa
     dy = stat.location.lat - lata
     return hypot(dx,dy)
 
-def findadd(stat, lata, longa):
-    wa = finddist(stat, lata, longa)
+
+def find_add(stat, lata, longa):
+    wa = find_dist(stat, lata, longa)
     const = 1
     return const/(wa**2)
 
-def pointcalc(lata,longa):
+
+def point_calc(lata,longa):
     i = 0.0
     for s in wmata.lines:
-        for stat in s:
-             
+        for staa in s:
+            i += find_add(staa, lata, longa)
+    return i
+

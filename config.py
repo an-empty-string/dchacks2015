5ccf75a887510f68731b61caf9e9e106b3d1c513
Fox Wilson <foxwilson123@gmail.com>
2015-08-01 17:06:39 -0400

    config: username -> user

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import json
 import utils.metro
+from peewee import *
 
 with open("config.json") as f:
     config = json.load(f)
 
 redis_info = {"host": config["REDIS_HOST"], "password": config["REDIS_PASSWORD"]}
 wmata = utils.metro.MetroApi(config["API_KEY"], redis_info)
+
+db = PostgresqlDatabase(config["POSTGRES_USER"], host=config["POSTGRES_HOST"],
+                        user=config["POSTGRES_USER"],
+                        password=config["POSTGRES_PASSWORD"])

742d6ce817e593ba6a5352711f32f71010d59710
James Woglom <j@wogloms.net>
2015-08-02 04:37:44 -0400

    add check for group

---
+++ scripts/fix_historical_data.py
@@ -0,0 +1,37 @@
+from models import HistoricalTrainPosition
+from config import wmata
+from datetime import datetime
+import json
+
+import os
+import os.path
+
+pos = list(HistoricalTrainPosition.select())
+updates = 0
+
+for tr in pos:
+
+    trackgroup = tr.trackgroup
+
+    if trackgroup is not None:
+        continue
+
+    """LEFT side of Metro map = 2 (incl Branch Ave)
+       RIGHT side of Metro map = 1 (incl Greenbelt)
+    """
+                        #ShGrv, Wihle, Vnna, FrncSp, Hntgn, BchAv 
+    if dest_station in ['A15', 'N06', 'K08', 'J03', 'C15', 'F11']:
+        trackgroup = 2
+                          #Glnmt, Grnblt, NCrl, Lrgo, Grnbt, MtVrn, FtTtn, FtTtn
+    elif dest_station in ['B11', 'E10', 'D13', 'G05', 'F11', 'E01', 'B06', 'E06']:
+        trackgroup = 1
+
+    if trackgroup is None:
+        continue
+
+    tr.trackgroup = trackgroup
+    updates += 1
+    
+    tr.save()
+
+print(updates, "updates")
--- scripts/save_current_data.py
+++ scripts/save_current_data.py
@@ -19,9 +19,20 @@ for train in all_trains:
 
     cars = train.cars
     time = train.time_int
-    group = train.group
+    try:
+        trackgroup = int(train.group)
+    except ValueError:
+        """LEFT side of Metro map = 2 (incl Branch Ave)
+           RIGHT side of Metro map = 1 (incl Greenbelt)
+        """
+                            #ShGrv, Wihle, Vnna, FrncSp, Hntgn, BchAv 
+        if dest_station in ['A15', 'N06', 'K08', 'J03', 'C15', 'F11']:
+            trackgroup = 2
+                              #Glnmt, Grnblt, NCrl, Lrgo, Grnbt, MtVrn, FtTtn, FtTtn
+        elif dest_station in ['B11', 'E10', 'D13', 'G05', 'F11', 'E01', 'B06', 'E06']:
+            trackgroup = 1
 
-    HistoricalTrainPosition.create(cars=cars, line_code=line_code, time=time, group=group,
+    HistoricalTrainPosition.create(cars=cars, line_code=line_code, time=time, trackgroup=track_group,
                                    next_station=next_station, dest_station=dest_station,
                                    timestamp=ts)
 

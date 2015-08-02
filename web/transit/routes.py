85bcd435d8299d50f5526dfcd6eca259bd5d9b75
Fox Wilson <foxwilson123@gmail.com>
2015-08-01 22:55:08 -0400

    add runweb

--- web/transit/routes.py
+++ web/transit/routes.py
@@ -1,13 +1,11 @@
-import transit
-import transit.views.basic
-
-from transit import app
+from . import app
+from .views import basic
 
 routes = (
-    ('/api/trains/', transit.views.basic.get_all_trains),
-    ('/api/trains/line/<line>/', transit.views.basic.get_trains_on_line),
-    ('/api/trains/station/<station>/', transit.views.basic.get_trains_at_station)
+    ('/api/trains/', basic.get_all_trains),
+    ('/api/trains/line/<line>/', basic.get_trains_on_line),
+    ('/api/trains/station/<station>/', basic.get_trains_at_station)
 )
 
-for route, func in routes:
-    app.add_url_rule(rule=route, endpoint.__name__, endpoint)
+for route, endpoint in routes:
+    app.add_url_rule(route, endpoint.__name__, endpoint)

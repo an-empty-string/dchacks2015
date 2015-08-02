23ce1023cd97375ab249e2739b8f4cd90ada2147
Fox Wilson <foxwilson123@gmail.com>
2015-08-02 02:25:12 -0400

    add stations and lines api routes

--- web/transit/routes.py
+++ web/transit/routes.py
@@ -6,6 +6,9 @@ routes = (
     ('/api/trains/', api.get_all_trains),
     ('/api/trains/line/<line>/', api.get_trains_on_line),
     ('/api/trains/station/<station_code>/', api.get_trains_at_station),
+    ('/api/station/<code>/', api.get_station_by_code),
+    ('/api/stations/', api.get_stations),
+    ('/api/lines/', api.get_lines),
     ('/', main.index)
 )
 
--- web/transit/views/api.py
+++ web/transit/views/api.py
@@ -1,17 +1,32 @@
 from flask import jsonify
 from config import wmata
 
-
 def _station_fmt(station):
     return {
         "name": station.name,
         "station_code": station.station_code
     }
 
+def _station_fmt_complete(station):
+    return {
+        "name": station.name,
+        "station_code": station.station_code,
+        "lat": station.location.lat,
+        "lon": station.location.lon,
+        "lines": [k.line_code for k in station.lines]
+    }
+
 def _line_fmt(line):
     return {
         "friendly_name": line.friendly_name,
         "line_code": line.line_code
+    }
+
+def _line_fmt_complete(line):
+    return {
+        "friendly_name": line.friendly_name,
+        "line_code": line.line_code,
+        "stations": [k.station_code for k in line.stations]
     }
 
 def get_all_trains():
@@ -65,4 +80,18 @@ def get_trains_at_station(station_code):
                 "time_int": tr.time_int
             })
     return jsonify(predictions=output)
+
+def get_station_by_code(code):
+    station = wmata.stations[code]
+    return jsonify(_station_fmt_complete(station))
+
+def get_line_by_code(code):
+    line = wmata.lines[code]
+    return jsonify(_line_fmt_complete(line))
+
+def get_stations():
+    return jsonify(stations=[_station_fmt_complete(i) for i in wmata.stations.all.values()])
+
+def get_lines():
+    return jsonify(lines=[_line(i) for i in wmata.lines.all.values()])
 

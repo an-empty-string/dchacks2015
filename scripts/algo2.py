4d52f5c40d6ad2ba1b20a58c5c6237b61a4ced39
James Woglom <j@wogloms.net>
2015-08-02 01:36:17 -0400

    radius

--- scripts/algo2.py
+++ scripts/algo2.py
@@ -5,12 +5,12 @@ csvdata = []
 for s in wmata.lines.all:
     for staa in wmata.lines[s].stations:
         if s == 'RD':
-            w00t = 4.5
+            headway = 4.5
         elif s == 'BL':
-            w00t = 12
+            headway = 12
         else:
-            w00t = 6
-        csvdata.append((staa.location.lat, staa.location.lon, 1/w00t))
+            headway = 6
+        csvdata.append((staa.location.lat, staa.location.lon, 1/headway))
 stringydata = "\n".join([",".join(list(map(str, i))) for i in csvdata])
 with open("data2.csv", "w") as f:
     f.write(stringydata)

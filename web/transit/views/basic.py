a96e888c9bc69ca7a9f43c5f413954ab9edd1bad
James Woglom <j@wogloms.net>
2015-08-01 23:49:19 -0400

    fix trains api

--- web/transit/views/basic.py
+++ web/transit/views/basic.py
@@ -48,25 +48,20 @@ def get_trains_on_line(line):
     return jsonify(predictions=output)
 
 def get_trains_at_station(station_code):
-    print("Loading predictions")
-    predictions = wmata.rail_system.all_trains()
-    print("Done")
     output = []
-    if station_code in wmata.stations.all:
-        station = wmata.stations[station_code]
-
-        if station in predictions:
-            trains = predictions[station]
-    
-            for tr in trains:
-                if tr.line.line_code == line:
-                    output.append({
-                        "destination": _station_fmt(tr.destination),
-                        "station": _station_fmt(tr.station),
-                        "cars": tr.cars,
-                        "line": _line_fmt(tr.line),
-                        "time": tr.time,
-                        "time_int": tr.time_int
-                    })
+    if station_code in wmata.stations:
+        print("Loading predictions")
+        station = wmata.stations[line_code]
+        trains = station.trains()
+        print("Done")
+        for tr in trains:
+            output.append({
+                "destination": _station_fmt(tr.destination),
+                "station": _station_fmt(tr.station),
+                "cars": tr.cars,
+                "line": _line_fmt(tr.line),
+                "time": tr.time,
+                "time_int": tr.time_int
+            })
     return jsonify(predictions=output)
 

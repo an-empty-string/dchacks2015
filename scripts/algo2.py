f0bc0f99c865dbed293f75da00a0201b6216ce93
laudiacay <laudiacay@gmail.com>
2015-08-02 02:50:07 -0400

    adding bus functionality to heatmap

--- scripts/algo2.py
+++ scripts/algo2.py
@@ -1,7 +1,11 @@
 __author__ = 'claudia'
 from config import wmata
-
+import csv
 csvdata = []
+with open('stops.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for x in reader:
+        csvdata.append(float(x[5]),float(x[6]),1/30)
 for s in wmata.lines.all:
     for staa in wmata.lines[s].stations:
         if s == 'RD':
@@ -10,7 +14,8 @@ for s in wmata.lines.all:
             headway = 12
         else:
             headway = 6
-        csvdata.append((staa.location.lat, staa.location.lon, 1/headway))
+        csvdata.append((staa.location.lat, staa.location.lon, 5/headway))
+
 stringydata = "\n".join([",".join(list(map(str, i))) for i in csvdata])
 with open("data2.csv", "w") as f:
     f.write(stringydata)
